02125bdefb8ff7cc94b55ea2c309cfe84f0db5f6
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 12:26:28 +0530

    if else statement in PHP

---
+++ conditional.php
@@ -0,0 +1,17 @@
+
+<?php 
+
+// $x = 10;
+// if($x > 30) {
+//     echo "X is Greater";
+// }else {
+//     echo "X is Smaller";
+// }
+
+$x = 10;
+if($x > 30) echo "X is Greater";
+else echo "X is Smaller";
+
+
+
+?>
