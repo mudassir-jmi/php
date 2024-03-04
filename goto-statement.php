abdc475887e41a16c2914f724daac4104d527cd0
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 13:52:53 +0530

    goto statement In PHP

---
+++ goto-statement.php
@@ -0,0 +1,15 @@
+
+<?php 
+// goto statement is a label when we use goto its jump the label of goto and skip all the code between them.
+
+echo "Hello Goto<br>";
+goto mycode;
+echo "Hello Goto<br>";
+echo "Hello Goto<br>";
+echo "Hello Goto<br>";
+echo "Hello Goto<br>";
+
+mycode:
+echo "Here is My Code";
+
+?>
