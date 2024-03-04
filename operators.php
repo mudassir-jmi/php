2a01b5e5e3e350847bf27e17add5ed5c8cc06007
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 12:10:28 +0530

    Assignment Operator in PHP

--- operators.php
+++ operators.php
@@ -10,6 +10,13 @@ Multiplication (*): $result = 10 * 5;
 Division (/): $result = 10 / 5;
 Modulus (%): $result = 10 % 3;
 
+Assignment Operators: 👇
+
+Assignment (=): $variable = 10;
+Addition Assignment (+=): $variable += 5;  // $variable = $variable + 5;
+Subtraction Assignment (-=): $variable -= 5;
+Multiplication Assignment (*=): $variable *= 5;
+Division Assignment (/=): $variable /= 5;
 
 */
 
