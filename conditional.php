0aae0f44d932b83ca76408f9f125e61fb3b59dbd
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 12:33:27 +0530

    elseif in PHP to check more condition

--- conditional.php
+++ conditional.php
@@ -16,5 +16,23 @@ $age = 20;
 $status = ($age >= 18) ? "Adult" : "Minor";
 echo $status; // Output: Adult
 
+// if , elseif, else
+
+$studentPercentage = 75;
+
+// Determine and print the grade
+echo "Student Percentage: $studentPercentage%\n";
+
+if ($studentPercentage >= 90) {
+    echo "Grade: A\n";
+} elseif ($studentPercentage >= 80) {
+    echo "Grade: B\n";
+} elseif ($studentPercentage >= 70) {
+    echo "Grade: C\n";
+} elseif ($studentPercentage >= 60) {
+    echo "Grade: D\n";
+} else {
+    echo "Grade: F\n";
+}
 
 ?>
