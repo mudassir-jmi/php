144326115a205067da0bdc6915a2f214d7564ffc
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 12:06:46 +0530

    arithmetic operator in php

---
+++ operators.php
@@ -0,0 +1,16 @@
+
+<?php 
+/* 
+
+Arithmetic Operators: 👇
+
+Addition (+): $result = 10 + 5;
+Subtraction (-): $result = 10 - 5;
+Multiplication (*): $result = 10 * 5;
+Division (/): $result = 10 / 5;
+Modulus (%): $result = 10 % 3;
+
+
+*/
+
+?>
