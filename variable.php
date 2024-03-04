07444ec24c499e061fa7c5b1806d834699ea5134
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-04 11:37:22 +0530

    main

---
+++ variable.php
@@ -0,0 +1,25 @@
+
+<!-- 
+    Variable is a container to hold the data in a program
+ -->
+ 
+ <?php 
+/*
+
+Write Way to Declare variable in PHP 👇
+ $name
+ $_name
+ $my_name
+ $my-name
+ $myName
+ $myname123
+
+*/
+?>
+
+<?php 
+$name = "Mudassir";
+echo $name;   // print the value
+echo "<h1>". $name ."</h1>";  // print using HTML tag
+echo "Hello : " . $name;
+?>
