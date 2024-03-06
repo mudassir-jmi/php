a9c7151a3020eb21e3870ccb31134a0f17f7174d
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-06 13:15:55 +0530

    array_replace and array_replace_recursive in PHP

--- array.php
+++ array.php
@@ -183,4 +183,34 @@ echo array_search('grapes', $food); // it's return 2 that's means index number o
 
 ?>
 
+<?php 
+// array_replace() 👉 it's used in Index or Associative Array
 
+// $frut = ['apple', 'grapes', 'banana'];
+// $veg = ['carrot', 'pea'];
+
+// $newArr = array_replace($frut, $veg);
+// echo "<pre>";
+// print_r($newArr);
+// echo "</pre>";
+
+$frut = ['apple', 'grapes', 'banana'];
+$veg = ['a'=>'carrot','b'=>'pea'];
+
+$newArr = array_replace($frut, $veg);
+echo "<pre>";
+print_r($newArr);
+echo "</pre>";
+
+// array_replace_recursive() 👉 it's used in Multidemensional Associative Array
+
+$array1 = array("a" =>array("red"), "b" => array("green", "pink"));
+$array2 = array("a" => array("yellow"), "b" => array("black"));
+
+$newArray = array_replace_recursive($array1, $array2);
+
+echo "<pre>";
+print_r ($newArray);
+echo "</pre>";
+
+?>
